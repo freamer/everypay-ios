0a34839b63ee840e56cdd016e57a95dde7d3189f
freamer <freamer89@gmail.com>
2016-08-09 13:05:46 +0300

    test

--- everyPay/sdk/Constants.m
+++ everyPay/sdk/Constants.m
@@ -8,7 +8,7 @@
 
 #import "Constants.h"
 
-NSString *const kEveryPayApiTesting = @"https://gw-staging.every-pay.com";
+NSString *const kEveryPayApiTesting = @"https://gw-demo.every-pay.com";
 NSString *const kEveryPayApiLive = @"http://gw.every-pay.com";
 NSString *const kMercantApiTesting = @"https://igwshop-staging.every-pay.com";
 
